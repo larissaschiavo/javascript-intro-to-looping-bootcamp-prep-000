c1263a584c087fa0b3ce180a3594b3bcab76dedb
Larissa Schiavo <larissafschiavo@gmail.com>
2017-08-31 20:47:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,11 +19,12 @@ function whileLoop(n) {
 }
 
 function maybeTrue() {
-return Math.random() >= 0.5
+  return Math.random() >= 0.5
 }
+
 function doWhileLoop(array) {
   do {
     array.pop()
   } while (array.length > 0 && maybeTrue());
-return array 
+return array
 }
